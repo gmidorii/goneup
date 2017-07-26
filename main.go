7769e3f2ad08626837bb616328245dcacfaa2b76
tagasouichiro <stitow14@gmail.com>
2017-07-26 23:39:36 +0900

    Add log

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 	http.HandleFunc("/index", indexHandler)
 
+	log.Println("run server port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalln(err)
 	}
