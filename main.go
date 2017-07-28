a7cf255063ae3927c959b2a4c704454235764f4a
tagasouichiro <stitow14@gmail.com>
2017-07-28 23:36:38 +0900

    Add post handler

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 	http.HandleFunc("/index", indexHandler)
+	http.HandleFunc("/post", postHandler)
 
 	log.Println("run server port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
